23a3934282f43644a6b1bdfb0246278d883c2f6e
Daniel de Lima <dlimapa@gmail.com>
2021-03-25 07:58:46 -0300

    Usando padding para separar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,29 @@ class FriendsApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meus amigos',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Meus amigos'),
-          ),
-          body: Center()),
+        appBar: AppBar(
+          title: Text('Meus amigos'),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.lightBlueAccent),
+                  child: Text('Maria', style: TextStyle(fontSize: 50)),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: DecoratedBox(
+                decoration: BoxDecoration(color: Colors.lightBlueAccent),
+                child: Text('Gabriel', style: TextStyle(fontSize: 50)),
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
