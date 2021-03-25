b9599f7b00e62c1b7664345f10425ec619cf1dc5
Daniel de Lima <dlimapa@gmail.com>
2021-03-25 07:32:32 -0300

    Projeto simples com statelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Livro sobre Flutter',
-      home: MyHomePage(),
-    );
-  }
+void main() {
+  runApp(FriendsApp());
 }
 
-class MyHomePage extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCount() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void alert(String message) {
-    print(message);
-  }
-
+class FriendsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Nosso aplicativo'),
-        backgroundColor: Colors.brown,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Vezes que o botão foi apertado',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCount,
-        child: Icon(Icons.add),
-        backgroundColor: Colors.red,
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.brown,
-        child: Container(
-            height: 100,
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.add, color: Colors.white),
-                      onPressed: () {
-                        alert('Adicionei qualquer coisa');
-                      }),
-                  IconButton(
-                      icon: Icon(Icons.add_a_photo, color: Colors.white),
-                      onPressed: () {
-                        alert('Adicionei uma foto');
-                      })
-                ],
-              ),
-            )),
-      ),
+    return MaterialApp(
+      title: 'Meus amigos',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Meus amigos'),
+          ),
+          body: Center()),
     );
   }
 }
