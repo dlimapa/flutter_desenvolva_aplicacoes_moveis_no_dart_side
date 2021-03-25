663a718730467b46284f2784bdbe064a853de313
Daniel de Lima <dlimapa@gmail.com>
2021-03-25 06:35:14 -0300

    Usando StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Framework',
-    home: TutorialHome(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Livro sobre Flutter',
+      home: MyHomePage(),
+    );
+  }
 }
 
-class TutorialHome extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCount() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void alert(String message) {
+    print(message);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Menu de navegação',
-          onPressed: null,
-        ),
-        title: Text('Exemplo de título'),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search), tooltip: 'Buscar', onPressed: null),
-        ],
+        title: Text('Nosso aplicativo'),
+        backgroundColor: Colors.brown,
       ),
       body: Center(
-        child: Text('Olá mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Vezes que o botão foi apertado',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        tooltip: 'Adicionar',
+        onPressed: _incrementCount,
         child: Icon(Icons.add),
-        onPressed: null,
+        backgroundColor: Colors.red,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.brown,
+        child: Container(
+            height: 100,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                      icon: Icon(Icons.add, color: Colors.white),
+                      onPressed: () {
+                        alert('Adicionei qualquer coisa');
+                      }),
+                  IconButton(
+                      icon: Icon(Icons.add_a_photo, color: Colors.white),
+                      onPressed: () {
+                        alert('Adicionei uma foto');
+                      })
+                ],
+              ),
+            )),
       ),
     );
   }
