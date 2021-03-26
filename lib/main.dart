e4b0fa11bb574e93c3edd7cba85e638ca7398e8a
Daniel de Lima <dlimapa@gmail.com>
2021-03-25 21:59:40 -0300

    Criando um widget stateless

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,27 @@ class FriendsApp extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-                padding: const EdgeInsets.all(10),
-                child: DecoratedBox(
-                  decoration: BoxDecoration(color: Colors.lightBlueAccent),
-                  child: Text('Maria', style: TextStyle(fontSize: 50)),
-                )),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: DecoratedBox(
-                decoration: BoxDecoration(color: Colors.lightBlueAccent),
-                child: Text('Gabriel', style: TextStyle(fontSize: 50)),
-              ),
-            ),
+            MyFriend('Maria'),
+            MyFriend('Gabriel'),
+            MyFriend('Anderson'),
           ],
         )),
+      ),
+    );
+  }
+}
+
+class MyFriend extends StatelessWidget {
+  final String text;
+  const MyFriend(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: Colors.lightBlueAccent),
+        child: Text(text, style: TextStyle(fontSize: 50)),
       ),
     );
   }
